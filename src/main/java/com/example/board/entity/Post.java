324ebab72fd66c62459aaed5aad8e8623d43bce8
hyeonjeong9911 <knk990101@naver.com>
2024-11-13 15:43:35 +0900

    refactor: Entity 제약 조건 설정

--- src/main/java/com/example/board/entity/Post.java
+++ src/main/java/com/example/board/entity/Post.java
@@ -1,20 +1,30 @@
 package com.example.board.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Date;
 
 @Entity
+@Table(name = "post")
 public class Post {
     @Id
+    @Column(name = "post_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long postId;
+    @Column(name = "title", nullable = false)
     public String title;
+    @Column(name = "content", nullable = false)
     public String content;
+    @Column(name = "author", nullable = false)
     public String author;
+    @Column(name = "created_at")
     public Date createdAt;
-    public Date updateAt;
+    @Column(name = "updated_at")
+    public Date updatedAt;
+    @Column(name = "view_count")
     public Integer viewCount;
 }
