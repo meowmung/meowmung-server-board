e7ee70bf41e23a48ab8bc964c9eb643c314ddef7
bbungjin <leejintwo12@naver.com>
2024-11-28 19:42:07 +0900

    feat: 필요 데이터 추가

--- src/main/java/com/example/board/post/dto/request/PostOneRequest.java
+++ src/main/java/com/example/board/post/dto/request/PostOneRequest.java
@@ -2,21 +2,29 @@ package com.example.board.post.dto.request;
 
 import com.example.board.comment.dto.request.CommentOneRequest;
 import com.example.board.post.entity.Post;
+import jakarta.persistence.Column;
+import java.util.Date;
 import java.util.List;
 
 public record PostOneRequest(
+        Long postId,
         String title,
         String content,
         String nickname,
+        Date createdAt,
+        Integer viewCount,
         List<CommentOneRequest> comments
 ) {
     public static PostOneRequest fromEntity(Post post) {
         List<CommentOneRequest> commentOneRequests = CommentOneRequest.fromEntity(post.getComments());
 
         return new PostOneRequest(
+                post.postId,
                 post.title,
                 post.content,
                 post.nickname,
+                post.createdAt,
+                post.viewCount,
                 commentOneRequests // 생각
         );
     }
